887708654414243d3a81802eba1dd0e54266d619
Justin Penny <justincpenny@hotmail.com>
2025-08-03 22:27:35 -0400

    added log component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,35 @@ import { useState } from 'react';
 
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from './components/Log';
 function App() {
   
   // If two components rely on a shared state hook you must lift the state to the parent component 
   // In this instance both the gameboard and the player component need the state of the game so we must use the parent App component to manage it
   // Now the active player is passed to both components so they each know whose turn it is 
   const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
 
-  function handleSelectedSquare(){
+  function handleSelectedSquare(rowIndex, colIndex){
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      // We create our own currentPlayer var so that we don't try and use activePlayer and mix states
+      if(prevTurns.length > 0 && prevTurns[0].player ==='X'){
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [
+        { square: {row: rowIndex, col: colIndex}, 
+          player: currentPlayer}, 
+          ...prevTurns];
+      
+      return updatedTurns;
+    });
   }
+
+
 
   return (
     <main>
@@ -20,8 +39,9 @@ function App() {
         <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
         <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
       </ol>
-        <GameBoard onSelectSquare={handleSelectedSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectedSquare} turns={gameTurns}/>
       </div>
+      <Log turns={gameTurns}/>
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -7,23 +5,29 @@ const initialGameBoard = [
 ];
 
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}){
+export default function GameBoard({onSelectSquare, turns}){
+    let gameBoard = initialGameBoard;
 
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    for(const turn of turns){
+        const {square, player} = turn;
+        const {row, col} = square;
 
-
+        gameBoard[row][col] = player;
+    }
     // in JS you should create a deep copy of an array instead of trying to edit the original directly
     // Similar reasoning as to why you pass a function instead of the old state as a variable. Passing a reference is safer 
     // This is updating the state in an immutable way and is best practice
-    function handleSelectedSquare(rowIndex, colIndex){
-        setGameBoard((prevGameBoard)=>{
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
+    // function handleSelectedSquare(rowIndex, colIndex){
+    //     setGameBoard((prevGameBoard)=>{
+    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
 
-        onSelectSquare(); // calls handleSelectedSquare in App component. Switches active player
-    }
+    //     onSelectSquare(); // calls handleSelectedSquare in App component. Switches active player
+    // }
+
+    // The above code is commented out because we are going to lift this to the app component so it can be shared by the Log.jsx component
 
     return(
         <ol id="game-board">
@@ -32,7 +36,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}){
                     <ol>
                     {row.map((playerSymbol, colIndex)=> (
                         <li key={colIndex}>
-                            <button onClick={() => handleSelectedSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                         </li>
                     ))}
                     </ol>
